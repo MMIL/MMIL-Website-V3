e385dd0e8c8ba70ca5cefc6af2c2a33b23bedcdd
Anurag Sachdeva <anurag.sach28@gmail.com>
2018-08-25 20:55:05 +0530

    changes

--- static_mmil/js/register.js
+++ static_mmil/js/register.js
@@ -5,10 +5,10 @@ window.onload = function(){
                 submitCallback: function(){
                     // alert("Submit: check dev tools console for more");
                     // be aware that this prevents default form submit.
-                    var formData = conversationalForm.getFormData();
-                    var formDataSerialized = conversationalForm.getFormData(true);
-                    console.log("Formdata:", formData);
-                    console.log("Formdata, serialized:", formDataSerialized);
+                    // var formData = conversationalForm.getFormData();
+                    // var formDataSerialized = conversationalForm.getFormData(true);
+                    // console.log("Formdata:", formData);
+                    // console.log("Formdata, serialized:", formDataSerialized);
                     conversationalForm.addRobotChatResponse("It was nice meeting you. See you at the recruitments. 😊😊😊");
                     conversationalForm.addRobotChatResponse("Good Bye!!!");
                 }
