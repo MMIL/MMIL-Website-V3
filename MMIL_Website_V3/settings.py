a22102beaa2a4936cc85174355667839a52c6d24
arch888 <dwevediar@gmail.com>
2018-11-07 12:54:34 +0530

    Message

--- MMIL_Website_V3/settings.py
+++ MMIL_Website_V3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ks(zk!dod1w6)miq)uvia*$rqu3)dl0go1sb5eo1f$aawv937='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
